4a1f1c27223526f86ef2c219eaca77e883c9319a
CosmeNicolas <nicomas7590@gmail.com>
2023-11-24 18:40:54 -0300

    agregue los get y set de la clase persona

--- js/generaciones.js
+++ js/generaciones.js
@@ -8,3 +8,79 @@ esMayorDeEdad: indica si es mayor de edad, devuelve un mensaje indicando que la 
 mostrarDatos: devuelve toda la información del objeto.
 Luego crea la interfaz necesaria para que el usuario pueda crear un objeto persona, permitiendo ingresar las propiedades mediante un formulario, también agregar los botones “mostrar generación”, es “mayor de edad” e indicar en un alert el resultado de la función correspondiente.
  */
+
+class Persona{
+  #nombre;
+  #edad;
+  #DNI;
+  #sexo;
+  #peso;
+  #altura;
+  #nacimiento
+
+
+  constructor(nombre, edad, DNI, sexo, peso, altura, nacimiento){
+    this.#nombre = nombre,
+    this.#edad = edad,
+    this.#DNI = DNI,
+    this.#sexo = sexo,
+    this.#peso = peso,
+    this.#altura = altura
+    this.#nacimiento = nacimiento
+  }
+  get nombre(){
+    return this.#nombre;
+  }
+
+  set nombre(nuevoNombre){
+    this.#nombre = nuevoNombre
+  }
+
+  get edad(){
+    return this.#edad
+  }
+
+  set edad(nuevaEdad){
+    this.#edad = nuevaEdad
+  }
+
+  get DNI(){
+    return this.#DNI;
+  }
+
+  set DNI(nuevoDNI){
+    this.#DNI = nuevoDNI;
+  }
+
+  get sexo(){
+    return this.#sexo;
+  }
+
+  set sexo(nuevoSexo){
+    this.#sexo = nuevoSexo;
+  }
+
+  get peso(){
+    return this.#peso;
+  }
+
+  set peso(nuevoPeso){
+    this.#peso = nuevoPeso;
+  }
+
+  get altura(){
+    return this.#altura;
+  }
+
+  set altura(nuevaAltura){
+    this.#altura = nuevaAltura;
+  }
+
+  get nacimiento(){
+    return this.#nacimiento;
+  }
+
+  set nacimiento(nuevoNacimiento){
+    this.#nacimiento = nuevoNacimiento;
+  }
+}
