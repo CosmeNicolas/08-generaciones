47ee56a9abc074392c8e6d8a780a117cc32f8693
CosmeNicolas <nicomas7590@gmail.com>
2023-12-25 12:46:25 -0300

    agregue un dic en una nueva seccion del html entre la seccion de la imageny el boton

---
+++ js/detalleGeneraciones.js
@@ -0,0 +1,5 @@
+const sectionDetalle = document.getElementById('sectionGeneraciones');
+console.log(sectionDetalle)
+const div = document.createElement('div');
+sectionDetalle.appendChild(div)
+div.textContent = 'aqui van los datos'
